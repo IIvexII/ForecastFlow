e9c5b33dc4f1b0fe90f117701f7b5a1f39cbf85e
Zafeer Hafeez <contact@zafeer.pk>
2025-02-13 00:30:38 +0500

    chore: add assets and given data

--- App.tsx
+++ App.tsx
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { Canvas, LinearGradient, Rect, vec } from "@shopify/react-native-skia";
+import { green } from "react-native-reanimated/lib/typescript/Colors";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Yooo</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Canvas style={{ flex: 1 }}>
+      <Rect x={0} y={0} width={200} height={200}>
+        <LinearGradient
+          start={vec(0, 0)}
+          end={vec(180, 180)}
+          colors={["red", "blue", "green"]}
+        />
+      </Rect>
+    </Canvas>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });

bcaad539aa5ef6f2dbf22ce5a39c0bf504e4e19c
Zafeer Hafeez <contact@zafeer.pk>
2025-02-19 15:54:42 +0500

    feat: add animation for weather info text on top

--- components/sections/WeatherInfo.tsx
+++ components/sections/WeatherInfo.tsx
@@ -4,6 +4,8 @@ import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { Weather } from "../../models/Weather";
 import { DEGREE_SYMBOL } from "../../utils/constants";
 import { StyleSheet } from "react-native";
+import { useBottomSheetPosition } from "../../context/BottomSheetPosition";
+import Animated, { Extrapolation, interpolate, useAnimatedStyle } from "react-native-reanimated";
 
 interface WeatherInfoProps {
   weather: Weather;
@@ -11,30 +13,59 @@ interface WeatherInfoProps {
 
 const WeatherInfo: React.FC<WeatherInfoProps> = ({ weather }) => {
   const { top } = useSafeAreaInsets();
+  const bottomSheetPosition = useBottomSheetPosition();
   const { city, condition, temperature, high, low } = weather;
+
+  // animation
+  // city text style
+  const animatedCityStyle = useAnimatedStyle(() => ({
+    marginTop: interpolate(bottomSheetPosition.value, [0, 1], [0, -20], Extrapolation.CLAMP),
+  }));
+  // temprature text style
+  const animatedTempStyle = useAnimatedStyle(() => ({
+    fontSize: interpolate(bottomSheetPosition.value, [0, 1], [96, 52], Extrapolation.CLAMP),
+    lineHeight: interpolate(bottomSheetPosition.value, [0, 1], [96, 52], Extrapolation.CLAMP),
+  }));
+  // condition text style
+  const animatedConditionStyle = useAnimatedStyle(() => ({
+    fontSize: interpolate(bottomSheetPosition.value, [0, 1], [36, 18], Extrapolation.CLAMP),
+    lineHeight: interpolate(bottomSheetPosition.value, [0, 1], [36, 18], Extrapolation.CLAMP),
+  }));
+  // low high text style
+  const animatedHighLowStyle = useAnimatedStyle(() => ({
+    fontSize: interpolate(bottomSheetPosition.value, [0, 1], [24, 18], Extrapolation.CLAMP),
+    lineHeight: interpolate(bottomSheetPosition.value, [0, 1], [24, 18], Extrapolation.CLAMP),
+  }));
+
+  //  container style of bottom info - temprature, condition, high, low
+  const animatedBottomInfoContainerStyle = useAnimatedStyle(() => ({
+    opacity: interpolate(bottomSheetPosition.value, [0, 0.5, 1], [1, 0, 1], Extrapolation.CLAMP),
+    flexDirection: bottomSheetPosition.value > 0.5 ? "row" : "column",
+    justifyContent: "space-between",
+    gap: 12,
+  }));
 
   return (
     <View style={[styles.container, { top: top + 40 }]}>
       <View>
-        <Text style={styles.cityText}>{city}</Text>
+        <Animated.Text style={[styles.cityText, animatedCityStyle]}>{city}</Animated.Text>
 
-        <Text style={styles.temperatureText}>
-          {temperature}
-          {DEGREE_SYMBOL}
-        </Text>
-
-        <Text style={styles.conditionText}>{condition}</Text>
-
-        <View style={styles.highLowContainer}>
-          <Text style={styles.highLowText}>
-            H:{high}
+        <Animated.View style={[animatedBottomInfoContainerStyle]}>
+          {/* Temprature Text */}
+          <Animated.Text style={[styles.temperatureText, animatedTempStyle]}>
+            {temperature}
             {DEGREE_SYMBOL}
-          </Text>
-          <Text style={styles.highLowText}>
-            L:{low}
-            {DEGREE_SYMBOL}
-          </Text>
-        </View>
+          </Animated.Text>
+
+          {/* condition high low container */}
+          <Animated.View>
+            <Animated.Text style={[styles.conditionText, animatedConditionStyle]}>{condition}</Animated.Text>
+            <Animated.View style={[styles.highLowContainer]}>
+              <Animated.Text style={[styles.highLowText, animatedHighLowStyle]}>H:{high}°</Animated.Text>
+              <Animated.Text style={[styles.highLowText, animatedHighLowStyle]}>L:{low}°</Animated.Text>
+            </Animated.View>
+          </Animated.View>
+        </Animated.View>
       </View>
     </View>
   );
@@ -54,6 +85,7 @@ const styles = StyleSheet.create({
     fontSize: 36,
     lineHeight: 36,
     color: "white",
+    textAlign: "center",
   },
   temperatureText: {
     alignSelf: "center",
@@ -77,7 +109,7 @@ const styles = StyleSheet.create({
   highLowText: {
     fontFamily: "SF-Regular",
     fontSize: 24,
-    lineHeight: 24,
+    lineHeight: 30,
     color: "white",
   },
 });

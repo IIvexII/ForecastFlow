8f8fd435242cb5d9e8391d8569c2d75a4711973e
Zafeer Hafeez <contact@zafeer.pk>
2025-02-25 17:48:35 +0500

    fix: background gradient sizing glitch

--- components/GradientBackground.tsx
+++ components/GradientBackground.tsx
@@ -1,24 +1,24 @@
 import React from "react";
-import { StyleSheet, useWindowDimensions } from "react-native";
+import { StyleSheet, useWindowDimensions, View } from "react-native";
 import { Canvas, LinearGradient, Rect, vec } from "@shopify/react-native-skia";
 
 const GradientBackground: React.FC = () => {
   const { width, height } = useWindowDimensions();
 
   return (
-    <Canvas style={styles.absolute}>
-      <Rect x={0} y={0} width={width} height={height}>
-        <LinearGradient start={vec(0, 0)} end={vec(width, height)} colors={["#2e335a", "#1c1b33"]} />
-      </Rect>
-    </Canvas>
+    <View style={[styles.absolute, { width, height }]}>
+      <Canvas style={{ flex: 1 }}>
+        <Rect x={0} y={0} width={width} height={height}>
+          <LinearGradient start={vec(0, 0)} end={vec(width, height)} colors={["#2e335a", "#1c1b33"]} />
+        </Rect>
+      </Canvas>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   absolute: {
     ...StyleSheet.absoluteFillObject,
-    width: "100%",
-    height: "100%",
   },
 });
 

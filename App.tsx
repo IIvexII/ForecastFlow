ba3c3efdc989cce7f2d0ef933946b611ef48d91d
Zafeer Hafeez <contact@zafeer.pk>
2025-03-02 07:36:33 +0500

    refactor: shift main srouce code into src to avoid cluttering

--- App.tsx
+++ App.tsx
@@ -1,7 +1,6 @@
 import { useCallback } from "react";
 import { useFonts } from "expo-font";
 import { StatusBar } from "expo-status-bar";
-import { useWindowDimensions } from "react-native";
 import * as SplashScreen from "expo-splash-screen";
 import { QueryClient } from "@tanstack/react-query";
 import { NavigationContainer } from "@react-navigation/native";
@@ -9,11 +8,10 @@ import { SafeAreaProvider } from "react-native-safe-area-context";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { PersistQueryClientProvider } from "@tanstack/react-query-persist-client";
 
-import RootNavigator from "./navigators/RootNavigator";
-import { WeatherProvider } from "./context/WeatherContext";
+import RootNavigator from "./src/navigators/RootNavigator";
+import { WeatherProvider } from "./src/context/WeatherContext";
 
-import asyncStoragePersister from "./utils/persister";
-import { BottomSheetPositionProvider } from "./context/BottomSheetPosition";
+import asyncStoragePersister from "./src/utils/persister";
 
 SplashScreen.preventAutoHideAsync();
 
@@ -30,34 +28,31 @@ const queryClient = new QueryClient({
 });
 
 const App: React.FC = () => {
-  const { height } = useWindowDimensions();
   const [fontsLoaded] = useFonts({
-    "SF-Thin": require("./assets/fonts/SF-Pro-Display-Thin.otf"),
-    "SF-Regular": require("./assets/fonts/SF-Pro-Display-Regular.otf"),
-    "SF-Semibold": require("./assets/fonts/SF-Pro-Display-Semibold.otf"),
+    "SF-Thin": require("./src/assets/fonts/SF-Pro-Display-Thin.otf"),
+    "SF-Regular": require("./src/assets/fonts/SF-Pro-Display-Regular.otf"),
+    "SF-Semibold": require("./src/assets/fonts/SF-Pro-Display-Semibold.otf"),
   });
 
   const onLayoutRootView = useCallback(async () => {
-    if (fontsLoaded && height > 0) await SplashScreen.hideAsync();
+    if (fontsLoaded) await SplashScreen.hideAsync();
   }, [fontsLoaded]);
 
-  if (!fontsLoaded || height <= 0) return null;
+  if (!fontsLoaded) return null;
 
   return (
-    <PersistQueryClientProvider client={queryClient} persistOptions={{ persister: asyncStoragePersister }}>
-      <WeatherProvider>
-        <BottomSheetPositionProvider>
+    <SafeAreaProvider onLayout={onLayoutRootView}>
+      <PersistQueryClientProvider client={queryClient} persistOptions={{ persister: asyncStoragePersister }}>
+        <WeatherProvider>
           <GestureHandlerRootView style={{ flex: 1 }}>
-            <SafeAreaProvider onLayout={onLayoutRootView}>
-              <NavigationContainer>
-                <RootNavigator />
-              </NavigationContainer>
-              <StatusBar style="light" />
-            </SafeAreaProvider>
+            <NavigationContainer>
+              <RootNavigator />
+            </NavigationContainer>
+            <StatusBar style="light" />
           </GestureHandlerRootView>
-        </BottomSheetPositionProvider>
-      </WeatherProvider>
-    </PersistQueryClientProvider>
+        </WeatherProvider>
+      </PersistQueryClientProvider>
+    </SafeAreaProvider>
   );
 };
 
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -14,6 +14,7 @@ import { useWeatherQuery } from "../hooks/useWeatherQuery";
 import { useWeather } from "../context/WeatherContext";
 
 import { getCurrentLocation } from "../services/locationService";
+import { BottomSheetPositionProvider } from "../context/BottomSheetPosition";
 
 const Home: React.FC = () => {
   const [modalVisible, setModalVisible] = React.useState(false);
@@ -61,7 +62,7 @@ const Home: React.FC = () => {
   }, []);
 
   return (
-    <>
+    <BottomSheetPositionProvider>
       <View>
         <WeatherCityModal
           modalVisible={modalVisible}
@@ -76,7 +77,7 @@ const Home: React.FC = () => {
         onAddLocationPress={handleAddLocation}
         onWeatherListPress={handleWeatherList}
       />
-    </>
+    </BottomSheetPositionProvider>
   );
 };
 
